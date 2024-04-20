0c4a5c88dad61f591bfb3a752f71d1e295e7ab9f
Mats Eriksson <mats.eriksson@mynumbers.com>
2024-04-20 20:33:27 +0200

    Added Api project

---
+++ MQuiz.Api/Api/Controllers/AuthorizationController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers;
+
+[ApiController]
+public class AuthorizationController : ControllerBase
+{
+    private readonly ILogger<WeatherForecastController> _logger;
+
+    public AuthorizationController(ILogger<WeatherForecastController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpPost]
+    public IActionResult CreateGame()
+    {
+
+    }
+}
